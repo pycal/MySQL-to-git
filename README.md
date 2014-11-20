d7d78b58075498c91290d3c5989a1ede1f031cea
pycal <cailen@kmsm.ca>
2014-11-20 11:04:58 -0500

    better comments and readme

--- README.md
+++ README.md
@@ -3,11 +3,18 @@ MySQL-to-git
 
 Using cron, schedule automated MySQL database backups to git
 
+I like this approach because I don't have to cleanup or version files on my system, and yet 
+
 ### Instructions
 
-1. Replace script tags (removing curly braces)
-2. Create a directory outside server webroot {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/}
-3. Place git-db-backup.sh file in /etc/cron.daily/ or add to crontab
-4. chmod 600 git-db-backup.sh
+1. Replace {DB_USERNAME}, {DB_PASSWORD}, AND {DB_NAME} tags (removing curly braces) in git-db-backup.sh
+2. Create {REPO_NAME} on github or bitbucket and replace {REPO_NAME} in git-db-backup.sh
+3. `cd /some/directory/outside/webroot`
+4. `git clone git@github.com:{GIT_USERNAME}/{REPO_NAME}.git`
+5. `cd {new_repo}`
+6. `pwd` and replace {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} with result
+7. Save file and place git-db-backup.sh file in /etc/cron.daily/ or add to crontab
+8. `chmod 600 git-db-backup.sh`
+9. (Optional) test script `bash git-db-backup.sh`
 
 
--- git-db-backup.sh
+++ git-db-backup.sh
@@ -2,8 +2,14 @@
 # backup a MySQL database to a git repo
 # remember to chmod 600 this file
 /bin/date
-/usr/bin/mysqldump --add-drop-table -h localhost -u {DB_USERNAME} -p{DB_PASSWORD} {DB_NAME} > {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/}db-backup.sql
 datestamp=`date +'%Y-%m-%d'`
-/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/}.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/} add {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/}db-backup.sql
-/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/}.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/} commit -m "\"$datestamp backup."\"
-/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/}.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR/} push --verbose -u {REPO_NAME} master
+
+# dump database
+/usr/bin/mysqldump --add-drop-table -h localhost -u {DB_USERNAME} -p{DB_PASSWORD} {DB_NAME} > {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/{DB_NAME}.bak.sql
+
+# git add .sql file
+/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} add {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/{DB_NAME}.bak.sql
+
+# git commit and push
+/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} commit -m "\"$datestamp backup."\"
+/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} push --verbose -u origin master
