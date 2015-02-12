f13aff3c1de3803e61b6c333ab323fb3836826ea
pycal <cailen@kmsm.ca>
2015-02-12 15:17:03 -0500

    changed config to env vars to make setup clearer and cleaner

--- git-db-backup.sh
+++ git-db-backup.sh
@@ -4,12 +4,24 @@
 /bin/date
 datestamp=`date +'%Y-%m-%d'`
 
+# eg export MYSQL_BACKUP_USER=db_user
+export DB_USER=
+
+# eg export MYSQL_BACKUP_PW=$upers3cret!
+export DB_PW=
+
+# eg export MYSQL_DB=db_name
+export DB_NAME=
+
+# eg export BACKUP_DIR=/var/www/vhosts/example.com/private
+export BACKUP_DIR=
+
 # dump database
-/usr/bin/mysqldump --add-drop-table -h localhost -u {DB_USERNAME} -p{DB_PASSWORD} {DB_NAME} > {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/{DB_NAME}.bak.sql
+/usr/bin/mysqldump --add-drop-table -h localhost -u $DB_USER -p$DB_PW $MYSQL_DB > $BACKUP_DIR/$DB_NAME.bak.sql
 
 # git add .sql file
-/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} add {/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/{DB_NAME}.bak.sql
+/usr/bin/git --git-dir=$BACKUP_DIR/.git --work-tree=$BACKUP_DIR add $BACKUP_DIR/$DB_NAME.bak.sql
 
 # git commit and push
-/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} commit -m "\"$datestamp backup."\"
-/usr/bin/git --git-dir={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR}/.git --work-tree={/FULLY/QUALIFIED/PATH/TO/BACKUP/DIR} push --verbose -u origin master
+/usr/bin/git --git-dir=$BACKUP_DIR/.git --work-tree=$BACKUP_DIR commit -m "\"$datestamp backup."\"
+/usr/bin/git --git-dir=$BACKUP_DIR/.git --work-tree=$BACKUP_DIR push --verbose -u origin master
