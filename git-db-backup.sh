2fa20056843e6e071148d0c52ab304031dbffe69
pycal <cailen@kmsm.ca>
2015-02-12 15:20:47 -0500

    changed config to env vars to make setup clearer and cleaner

--- git-db-backup.sh
+++ git-db-backup.sh
@@ -10,14 +10,14 @@ export DB_USER=
 # eg export MYSQL_BACKUP_PW=$upers3cret!
 export DB_PW=
 
-# eg export MYSQL_DB=db_name
+# eg export DB_NAME=db_name
 export DB_NAME=
 
 # eg export BACKUP_DIR=/var/www/vhosts/example.com/private
 export BACKUP_DIR=
 
 # dump database
-/usr/bin/mysqldump --add-drop-table -h localhost -u $DB_USER -p$DB_PW $MYSQL_DB > $BACKUP_DIR/$DB_NAME.bak.sql
+/usr/bin/mysqldump --add-drop-table -h localhost -u $DB_USER -p$DB_PW $DB_NAME > $BACKUP_DIR/$DB_NAME.bak.sql
 
 # git add .sql file
 /usr/bin/git --git-dir=$BACKUP_DIR/.git --work-tree=$BACKUP_DIR add $BACKUP_DIR/$DB_NAME.bak.sql
